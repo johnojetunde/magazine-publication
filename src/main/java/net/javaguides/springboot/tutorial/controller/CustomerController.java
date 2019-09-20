43e3f094474589a3abcd2ed23bccdfa1518b0b8b
John Ojetunde <johnojetunde63@gmail.com>
2019-09-20 15:20:22 +0100

    1. Added Customer Entity and changed the default port to 9000

---
+++ src/main/java/net/javaguides/springboot/tutorial/controller/CustomerController.java
@@ -0,0 +1,78 @@
+package net.javaguides.springboot.tutorial.controller;
+
+import net.javaguides.springboot.tutorial.entity.Customer;
+import net.javaguides.springboot.tutorial.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/customers/")
+public class CustomerController {
+
+    private static final String staticFileDirectory = "customer/";
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping("signup")
+    public String showSignUpForm(Customer customer) {
+        return staticFileDirectory + "add-customer";
+    }
+
+    @GetMapping("list")
+    public String showUpdateForm(Model model) {
+        model.addAttribute("customers", customerRepository.findAll());
+        return "index";
+    }
+
+    @PostMapping("add")
+    public String addCustomer(@Valid Customer customer, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return staticFileDirectory + "add-customer";
+        }
+
+        customerRepository.save(customer);
+        return "redirect:list";
+    }
+
+    @GetMapping("edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid customer Id:" + id));
+        model.addAttribute("customer", customer);
+        return staticFileDirectory + "update-customer";
+    }
+
+    @PostMapping("update/{id}")
+    public String updateCustomer(@PathVariable("id") long id, @Valid Customer customer, BindingResult result,
+                                 Model model) {
+        if (result.hasErrors()) {
+            customer.setId(id);
+            return staticFileDirectory + "update-customer";
+        }
+
+        customerRepository.save(customer);
+        model.addAttribute("customers", customerRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteCustomer(@PathVariable("id") long id, Model model) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid customer Id:" + id));
+        customerRepository.delete(customer);
+        model.addAttribute("customers", customerRepository.findAll());
+        return "index";
+    }
+}
---
+++ src/main/java/net/javaguides/springboot/tutorial/entity/Customer.java
@@ -0,0 +1,51 @@
+package net.javaguides.springboot.tutorial.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Column(name = "name")
+    private String name;
+
+    @NotBlank(message = "Address is mandatory")
+    @Column(name = "address", length = 40)
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
